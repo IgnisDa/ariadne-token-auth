aa9ce5ad915835ea44a55de8ec8a9c175d80f35d
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-22 21:38:14 +0550

    feat(resolvers.py): add resolvers to create and delete antokens

--- example_project/example_app/api/resolvers.py
+++ example_project/example_app/api/resolvers.py
@@ -1,11 +1,27 @@
 from ariadne import MutationType, QueryType
-from ariadne_token_auth.decorators import login_required_ariadne
+from ariadne_token_auth.api import resolvers
+from ariadne_token_auth.decorators import login_required
 
 query = QueryType()
 mutation = MutationType()
 
+auth_mutation = MutationType()
+
+auth_mutation.set_field("getAuthToken", resolvers.get_auth_token)
+auth_mutation.set_field("deleteAuthToken", resolvers.delete_auth_token)
+
+auth_type_definitions = [
+    resolvers.type_defs,
+]
+
 
 @query.field("testQuery")
-@login_required_ariadne
+@login_required
 def test_query(self, info, *args, **kwargs):
-    return "The view worked successfully!"
+    return {"user": info.context.get("request").user}
+
+
+@mutation.field("testMutation")
+@login_required
+def test_mutation(self, info, *args, **kwargs):
+    return {"user": info.context.get("request").user}
