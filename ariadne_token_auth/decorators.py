2f819055bb36c39f92d0e04b194e2cb4aae9a20f
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-22 12:50:43 +0550

    feat(decorators.py): add decorator for ariadne

--- ariadne_token_auth/decorators.py
+++ ariadne_token_auth/decorators.py
@@ -1,6 +1,6 @@
 from functools import wraps
 
-from django.core import exceptions
+from . import exceptions
 
 
 def user_passes_test(test_func):
@@ -10,8 +10,36 @@ def user_passes_test(test_func):
             if test_func(request.user):
                 return f(request, *args, **kwargs)
             raise exceptions.PermissionDenied()
+
         return wrapper
+
+    return decorator
+
+
+def context(f):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            info = args[f.__code__.co_varnames.index("info")]
+            return func(info.context, *args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def user_passes_test_ariadne(test_func):
+    def decorator(f):
+        @wraps(f)
+        @context(f)
+        def wrapper(context, *args, **kwargs):
+            if test_func(context.get("request").user):
+                return f(*args, **kwargs)
+            raise exceptions.PermissionDenied()
+
+        return wrapper
+
     return decorator
 
 
 login_required = user_passes_test(lambda u: u.is_authenticated)
+login_required_ariadne = user_passes_test_ariadne(lambda u: u.is_authenticated)
