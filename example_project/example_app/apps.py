fdab84f3d6bca6ad301f7c67f916afd7141d5a85
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-22 12:49:55 +0550

    feat(apps.py): add autoreloader

--- example_project/example_app/apps.py
+++ example_project/example_app/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
+from django.conf import settings
+from django.utils.autoreload import autoreload_started
+
+
+def schema_watchdog(sender, **kwargs):
+    sender.watch_dir(settings.BASE_DIR, "**/*.graphql")
 
 
 class ExampleAppConfig(AppConfig):
-    name = 'example_app'
+    name = "example_app"
+
+    def ready(self):
+        autoreload_started.connect(schema_watchdog)
