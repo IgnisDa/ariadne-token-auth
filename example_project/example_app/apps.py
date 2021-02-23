371c6f8eec43e5545f04f7b413610e3ce9100453
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-23 09:47:43 +0550

    docs(apps.py): add comments to explain some obscure fucntions

--- example_project/example_app/apps.py
+++ example_project/example_app/apps.py
@@ -4,6 +4,10 @@ from django.utils.autoreload import autoreload_started
 
 
 def schema_watchdog(sender, **kwargs):
+    """This allows us to add all `*.graphql` files to django's autoreloader,
+    saving us of having to go through the hassle of restarting django's
+    development server everytime we make a change to our graphql schema. Don't
+    forget to add lines 17 and 18 too."""
     sender.watch_dir(settings.BASE_DIR, "**/*.graphql")
 
 
