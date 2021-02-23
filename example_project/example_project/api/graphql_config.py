d17a7b370f7bb16b3faaffec1ba7e2180c85d50a
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-23 09:48:00 +0550

    docs(graphql_config.py): add comments to explain fucntions

--- example_project/example_project/api/graphql_config.py
+++ example_project/example_project/api/graphql_config.py
@@ -8,6 +8,9 @@ from ariadne import (
 from django.conf import settings
 from example_app.api import resolvers as example_app_resolvers
 
+# assuming all our schema is defined in files which match the following pattern,
+# we can use python `glob` to import these files dynamically, so that we don't have
+# to specify each file here specifically.
 type_defs = [
     load_schema_from_path(f)
     for f in glob.glob(str(settings.BASE_DIR / "*" / "api" / "schema" / "*.graphql"))
