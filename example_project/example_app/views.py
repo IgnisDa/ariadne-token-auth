abd3e3fc43e04e60333834e87171a42a349e197a
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-23 09:47:23 +0550

    feat(urls.py, views.py): remove useless files

--- example_project/example_app/views.py
+++
@@ -1,7 +0,0 @@
-from ariadne_token_auth.decorators import login_required
-from django.http import JsonResponse
-
-
-@login_required
-def test_view(request):
-    return JsonResponse({"message": "The view worked successfully!", "status": True})
