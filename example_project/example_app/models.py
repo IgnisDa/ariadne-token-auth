19f637eda157c3b80b3a658823c94cdaedd195a4
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-23 10:32:48 +0550

    feat(managers.py, 0001_initial.py, models.py): add custom user model

--- example_project/example_app/models.py
+++ example_project/example_app/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from . import managers
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(
+        max_length=150, help_text=_("The username of the user."), unique=True
+    )
+    email = models.EmailField(help_text=_("Email of the user."), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = managers.CustomUserManager()
+
+    class Meta:
+        ordering = ("id",)
+
+    def __str__(self):
+        return f"{self.email}'s account"
