48487c562d22b0956213275ee65efa1ad09923d1
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-23 09:37:40 +0550

    feat(exceptions.py, models.py): replace gettext with gettext_lazy

--- ariadne_token_auth/models.py
+++ ariadne_token_auth/models.py
@@ -3,7 +3,7 @@ import os
 
 from django.conf import settings
 from django.db import models
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 from . import utils
 
