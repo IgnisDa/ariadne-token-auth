da1b88bde73067e21e3bdcda159f4c2cd4dd70a1
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-23 09:48:11 +0550

    feat(urls.py): remove url for example_app

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -1,11 +1,10 @@
 from ariadne.contrib.django.views import GraphQLView
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 from example_project.api.graphql_config import schema
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("graphql/", GraphQLView.as_view(schema=schema), name="graphql"),
-    path("", include("example_app.urls")),
 ]
